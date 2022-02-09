b4c1aa78bc40e2877f268bf2a1cf5c7a1712d35d
Dmakayev <dimitri.makayev@gmail.com>
2022-02-09 16:12:40 -0600

    - Update Calendar.vue - Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,21 @@
 
 
     <v-app-bar
-      app
-      color="primary"
-      dark
+        app
+        color="primary"
+        dark
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://campus.und.edu/brand/_files/images/logotype-full-rgb.png"
-          transition="scale-transition"
-          width="250"
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://campus.und.edu/brand/_files/images/logotype-full-rgb.png"
+            transition="scale-transition"
+            width="250"
         />
 
       </div>
-
 
 
       <v-spacer></v-spacer>
@@ -26,15 +25,38 @@
     </v-app-bar>
 
 
-
     <v-main>
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/contacts">Contact List</router-link> |
-        <router-link to="/calendar">Calendar</router-link> |
-        <router-link to="/cs-courses">CS Courses</router-link> |
-        <router-link to="/ee-courses">EE Courses</router-link> |
-        <router-link to="/FactoryCenter">Factory Center</router-link>
+      <div class="text-center">
+        <v-btn class="mx-2"
+               depressed
+               elevation="2"
+               to="/">Home
+        </v-btn>
+        <v-btn class="mx-2"
+               depressed
+               elevation="2"
+               to="/contacts">Contacts
+        </v-btn>
+        <v-btn class="mx-2"
+               depressed
+               elevation="2"
+               to="/calendar">Calendar
+        </v-btn>
+        <v-btn class="mx-2"
+               depressed
+               elevation="2"
+               to="/cs-courses">CS Courses
+        </v-btn>
+        <v-btn class="mx-2"
+               depressed
+               elevation="2"
+               to="/ee-courses">EE Courses
+        </v-btn>
+        <v-btn class="mx-2"
+               depressed
+               elevation="2"
+               to="/factory-center">Factory Center
+        </v-btn>
       </div>
       <router-view/>
     </v-main>
