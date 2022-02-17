73eed3df64896adb49243aa4885b9a085b9fd976
tjballworm <44422907+tjballworm@users.noreply.github.com>
2022-02-16 21:03:11 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -48,14 +48,8 @@
         <v-btn class="mx-2" depressed elevation="2" to="/cs-courses"
           >CS Courses
         </v-btn>
-        <v-btn class="mx-2" depressed elevation="2" to="/ee-courses"
-          >EE Courses
-        </v-btn>
         <v-btn class="mx-2" depressed elevation="2" to="/help"
         >Help
-        </v-btn>
-        <v-btn class="mx-2" depressed elevation="2" to="/factory-center"
-          >Factory Center
         </v-btn>
       </div>
       <router-view />
